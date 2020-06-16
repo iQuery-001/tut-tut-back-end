c4fcac5a21958a1392e47af86236092987371e24
Tsura <kota@vaga.tk>
2020-06-16 16:24:20 -0500

    session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_tut-tut", domain: "https://tut-tut.netlify.app"
+if Rails.env === 'production'
+  Rails.application.config.session_store :cookie_store, key: '_tut-tut', domain: 'https://tut-tut.netlify.app'
+else
+  Rails.application.config.session_store :cookie_store, key: '_your-app-name'
+end
